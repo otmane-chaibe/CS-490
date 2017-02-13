35d3a75da6277c0c13adbf748c11ebc825f5968e
Maurice Achtenhagen <maurice@achtenhagen.me>
2017-02-13 12:40:56 -0500

    Add cURL redirect option

--- Front-end/dologin.php
+++ Front-end/dologin.php
@@ -25,11 +25,12 @@
 	# cURL Request - Middle-end
 	$curl = curl_init();
 	curl_setopt_array($curl, [
+		CURLOPT_FOLLOWLOCATION => true,
 	    CURLOPT_RETURNTRANSFER => 1,
-	    CURLOPT_URL => 'https://web.njit.edu/~sma76',
-	    CURLOPT_USERAGENT => 'NJIT Auth Front-end',
-	    CURLOPT_POST => 1,
-	    CURLOPT_POSTFIELDS => [
+	    CURLOPT_URL            => 'https://web.njit.edu/~sma76/index.php',
+	    CURLOPT_USERAGENT      => 'NJIT Auth Front-end',
+	    CURLOPT_POST           => 1,
+	    CURLOPT_POSTFIELDS     => [
 	        "ucid" => $ucid,
 	        "pass" => $pass
 	    ]
