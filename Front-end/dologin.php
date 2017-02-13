361d11bc265f4cdb6a8957136af81b06d0a9cf9f
Maurice Achtenhagen <maurice@achtenhagen.me>
2017-02-13 12:49:19 -0500

    Fix bad output

--- Front-end/dologin.php
+++ Front-end/dologin.php
@@ -26,17 +26,17 @@
 	$curl = curl_init();
 	curl_setopt_array($curl, [
 		CURLOPT_FOLLOWLOCATION => true,
-	    CURLOPT_RETURNTRANSFER => 1,
-	    CURLOPT_URL            => 'https://web.njit.edu/~sma76/index.php',
-	    CURLOPT_USERAGENT      => 'NJIT Auth Front-end',
-	    CURLOPT_POST           => 1,
-	    CURLOPT_POSTFIELDS     => [
+		CURLOPT_RETURNTRANSFER => 1,
+		CURLOPT_URL            => 'https://web.njit.edu/~sma76/index.php',
+		CURLOPT_USERAGENT      => 'NJIT Auth Front-end',
+		CURLOPT_POST           => 1,
+		CURLOPT_POSTFIELDS     => [
 	        "ucid" => $ucid,
 	        "pass" => $pass
 	    ]
 	]);
 	$resp = curl_exec($curl);
 	curl_close($curl);
-	echo json_encode($resp);
+	echo $resp;
 
 ?>
