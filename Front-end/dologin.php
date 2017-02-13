a224a78c996b3989570be00009d178bbee734a8a
Maurice Achtenhagen <maurice@achtenhagen.me>
2017-02-13 11:56:31 -0500

    Refactor code

--- Front-end/dologin.php
+++ Front-end/dologin.php
@@ -17,11 +17,16 @@
 		header($_SERVER['SERVER_PROTOCOL'] . ' 400 Bad Request');
 		die(json_encode(["status" => 400, "response" => "Bad Request"]));
 	}
+	if (strlen($ucid) > 6 || strlen($pass) > 20) {
+		header($_SERVER['SERVER_PROTOCOL'] . ' 400 Bad Request');
+		die(json_encode(["status" => 400, "response" => "Bad Request"]));
+	}
 
+	# cURL Request - Middle-end
 	$curl = curl_init();
 	curl_setopt_array($curl, [
 	    CURLOPT_RETURNTRANSFER => 1,
-	    CURLOPT_URL => 'https://web.njit.edu/~ks492/searchform.php', # https://web.njit.edu/~sma76
+	    CURLOPT_URL => 'https://web.njit.edu/~sma76',
 	    CURLOPT_USERAGENT => 'NJIT Auth Front-end',
 	    CURLOPT_POST => 1,
 	    CURLOPT_POSTFIELDS => [
@@ -29,7 +34,8 @@
 	        "pass" => $pass
 	    ]
 	]);
-	echo json_encode(curl_exec($curl));
+	$resp = curl_exec($curl);
 	curl_close($curl);
+	echo json_encode($resp);
 
 ?>
