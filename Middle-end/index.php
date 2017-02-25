b7a395c6f3c7c13de657337f10536b2a0738fbca
saadanwar9 <saadanwar9@gmail.com>
2017-02-25 18:59:38 -0500

    Add files via upload
    I didnt understand how to add the arg type. So help please.

--- Middle-end/index.php
+++ Middle-end/index.php
@@ -1,44 +1,20 @@
-
 <?php
-	
+	/**
 	header('Content-Type: application/json');
-
 	# Check Request Method
 	if ($_SERVER['REQUEST_METHOD'] != "POST") {
 		header($_SERVER['SERVER_PROTOCOL'] . ' 405 Method Not Allowed');
 		header('Allow: POST');
 		die(json_encode(["status" => 405, "response" => "Must Use POST"]));
 	}
-
 	$ucid = $_POST['ucid'];
 	$pass = $_POST['pass'];
-
 	# Check POST Variables
 	if (empty($ucid) || empty($pass)) {
 		header($_SERVER['SERVER_PROTOCOL'] . ' 400 Bad Request');
 		die(json_encode(["status" => 400, "response" => "Bad Request"]));
 	}
 	
-	# cURL Request - NJIT
-	$curl = curl_init();
-	curl_setopt_array($curl, [
-		CURLOPT_FOLLOWLOCATION => true,
-		CURLOPT_HTTPHEADER     => ['Content-Type: application/x-www-form-urlencoded'],
-		CURLOPT_POST           => 1,
-		CURLOPT_POSTFIELDS     => "user=" . $ucid . "&pass=" . $pass . "&uuid=0xACA021",
-		CURLOPT_RETURNTRANSFER => 1,
-		CURLOPT_URL            => 'https://cp4.njit.edu/cp/home/login',
-		CURLOPT_USERAGENT      => 'NJIT Auth Middle-end'
-	]);
-	$resp = curl_exec($curl);
-	curl_close($curl);
-	
-	# Verify cURL response
-	$njit = ["status" => 403, "response" => "Login not successful"];
-	if (strpos($resp, "Login Successful")) {
-		$njit = ["status" => 200, "response" => "Login successful"];
-	}
-
 	# cURL Request - Back-end
 	$curl = curl_init();
 	curl_setopt_array($curl, [
@@ -63,7 +39,26 @@
 	
 	# Return Results
 	echo json_encode([
-		"njit" => $njit,
 		"db"   => $db
 	]);
+	
+	$methodName = $_POST['methodName'];
+	$argNum = $_POST['argNum'];
+	$conType = $_POST['ConType'];
+	**/
+	$output = $POST['output'];
+	echo "output is $output";
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	
