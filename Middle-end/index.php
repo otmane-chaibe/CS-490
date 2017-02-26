58392962ac325861fa7783a27e45b25ba2bca0b2
saadanwar9 <saadanwar9@gmail.com>
2017-02-25 19:00:58 -0500

    Add files via upload
    I didnt understand how to add the arg type. So help please.

--- Middle-end/index.php
+++ Middle-end/index.php
@@ -1,5 +1,5 @@
 <?php
-	/**
+	
 	header('Content-Type: application/json');
 	# Check Request Method
 	if ($_SERVER['REQUEST_METHOD'] != "POST") {
@@ -11,54 +11,80 @@
 	$pass = $_POST['pass'];
 	# Check POST Variables
 	if (empty($ucid) || empty($pass)) {
-		header($_SERVER['SERVER_PROTOCOL'] . ' 400 Bad Request');
-		die(json_encode(["status" => 400, "response" => "Bad Request"]));
+		$methodName = $_POST['methodName'];
+		$argNum = $_POST['argNum'];
+		$conType = $_POST['ConType'];
+		$methodInput = $_POST['methodInput'];
+		$methodOutput = $_POST['methodOutput'];
+		
+		if (empty($methodName) || empty($argNum) || empty($conType) || empty($methodInput) || empty($methodOutput)) {
+			header($_SERVER['SERVER_PROTOCOL'] . ' 400 Bad Request');
+			die(json_encode(["status" => 400, "response" => "Bad Request"]));
+		}
+		
+		# cURL Request - Back-end = For Question
+		$curl = curl_init();
+		curl_setopt_array($curl, [
+			CURLOPT_RETURNTRANSFER => 1,
+			CURLOPT_URL            => 'https://web.njit.edu/~ks492/searchform.php',
+			CURLOPT_USERAGENT      => 'NJIT Auth Middle-end',
+			CURLOPT_POST           => 1,
+			CURLOPT_POSTFIELDS     => [
+				"argNum" => $argNum,
+				"methodName" => $methodName,
+				"conType" => $conType,
+				"methodInput" => $methodInput,
+				"methodOutput" =>  $methodOutput
+			]
+		]);
+		
+		$resp = curl_exec($curl);
+		curl_close($curl);
+		
+		# Verify cURL response
+		$resp = json_decode($resp, true);
+		$db = ["status" => 403, "response" => "Login not successful"];
+		if (!empty($resp["db"]) && $resp["db"]["status"] == 200) {
+			$db = ["status" => 200, "response" => "Login successful"];
+		}
+		
+		# Return Results
+		echo json_encode([
+			"db"   => $db
+		]);
+	
+	
 	}
-	
-	# cURL Request - Back-end
-	$curl = curl_init();
-	curl_setopt_array($curl, [
-	    CURLOPT_RETURNTRANSFER => 1,
-	    CURLOPT_URL            => 'https://web.njit.edu/~ks492/searchform.php',
-	    CURLOPT_USERAGENT      => 'NJIT Auth Middle-end',
-	    CURLOPT_POST           => 1,
-	    CURLOPT_POSTFIELDS     => [
-	    	"ucid" => $ucid,
-	       	"pass" => $pass
-	    ]
-	]);
-	$resp = curl_exec($curl);
-	curl_close($curl);
-	
-	# Verify cURL response
-	$resp = json_decode($resp, true);
-	$db = ["status" => 403, "response" => "Login not successful"];
-	if (!empty($resp["db"]) && $resp["db"]["status"] == 200) {
-		$db = ["status" => 200, "response" => "Login successful"];
+	else{
+		
+		# cURL Request - Back-end - For Login
+		$curl = curl_init();
+		curl_setopt_array($curl, [
+			CURLOPT_RETURNTRANSFER => 1,
+			CURLOPT_URL            => 'https://web.njit.edu/~ks492/searchform.php',
+			CURLOPT_USERAGENT      => 'NJIT Auth Middle-end',
+			CURLOPT_POST           => 1,
+			CURLOPT_POSTFIELDS     => [
+				"ucid" => $ucid,
+				"pass" => $pass
+			]
+		]);
+		$resp = curl_exec($curl);
+		curl_close($curl);
+		
+		# Verify cURL response
+		$resp = json_decode($resp, true);
+		$db = ["status" => 403, "response" => "Login not successful"];
+		if (!empty($resp["db"]) && $resp["db"]["status"] == 200) {
+			$db = ["status" => 200, "response" => "Login successful"];
+		}
+		
+		# Return Results
+		echo json_encode([
+			"db"   => $db
+		]);
 	}
-	
-	# Return Results
-	echo json_encode([
-		"db"   => $db
-	]);
-	
-	$methodName = $_POST['methodName'];
-	$argNum = $_POST['argNum'];
-	$conType = $_POST['ConType'];
-	**/
-	$output = $POST['output'];
-	echo "output is $output";
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
+
+?>
+	############################################################################
 	
