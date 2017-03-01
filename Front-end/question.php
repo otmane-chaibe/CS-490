02c4044fc41c560c3307042f2961e4a3c45d1ee3
Maurice Achtenhagen <maurice@achtenhagen.me>
2017-03-01 11:45:47 -0500

    Update question UI

--- Front-end/question.php
+++ Front-end/question.php
@@ -2,37 +2,54 @@
 
 <div id="question-wrapper">
 	<h1>Create Question</h1>
-	Question type
-	<select id="question-type">
-		<option value="0" selected>Conditional</option>
-		<option value="1">Control Flow</option>
-		<option value="2">Recursion</option>
-	</select>
-	Difficulty
-	<select id="question-difficulty">
-		<option value="0" selected>Easy</option>
-		<option value="1">Medium</option>
-		<option value="2">Difficult</option>
-	</select>
-	<br /><br />
-	Write a function named<input id="function-name" type="text">of type
-	<select id="func-type">
-		<option value="0" selected>Int</option>
-		<option value="1">Float</option>
-		<option value="2">Double</option>
-		<option value="3">String</option>
-		<option value="4">Bool</option>
-	</select>using
-	<input id="arg-count" type="text">args of type
-	<select id="arg-type">
-		<option value="0" selected>Int</option>
-		<option value="1">Float</option>
-		<option value="2">Double</option>
-		<option value="3">String</option>
-		<option value="4">Bool</option>
-	</select>
-	<textarea id="solution"></textarea>
-	<button id="submit" type="button" class="green">Submit</button>
+	<div class="step">
+		<div class="num">1</div>
+		Question type
+		<select id="question-type">
+			<option value="0" selected>Conditional</option>
+			<option value="1">Control Flow</option>
+			<option value="2">Recursion</option>
+		</select>
+		Difficulty
+		<select id="question-difficulty">
+			<option value="0" selected>Easy</option>
+			<option value="1">Medium</option>
+			<option value="2">Difficult</option>
+		</select>
+	</div>
+	<div class="step">
+		<div class="num">2</div>
+		Function Name<input id="function-name" type="text">Function Type
+		<select id="func-type">
+			<option value="0" selected>Int</option>
+			<option value="1">Float</option>
+			<option value="2">Double</option>
+			<option value="3">String</option>
+			<option value="4">Bool</option>
+		</select>
+	</div>
+	<div class="step">
+		<div class="num">3</div>
+		<p>Function arguments (0)</p>
+		Argument Name<input id="arg-name" type="text">Argument Type
+		<select id="arg-type">
+			<option value="0" selected>Int</option>
+			<option value="1">Float</option>
+			<option value="2">Double</option>
+			<option value="3">String</option>
+			<option value="4">Bool</option>
+		</select>
+		<button id="add-arg" type="button" class="blue">Add Argument</button>
+	</div>
+	<div class="step">
+		<div class="num">4</div>
+		<p>Question Solution</p>
+		<textarea id="solution"></textarea>
+	</div>
+	<div class="step">
+		<div class="num">5</div>
+		<button id="submit" type="button" class="green">Submit</button>
+	</div>
 </div>
 <div id="sidebar"></div>
 
