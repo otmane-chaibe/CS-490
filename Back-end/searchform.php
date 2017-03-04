50a2942d07516648bce120115e19711f374e5122
Maurice Achtenhagen <maurice@achtenhagen.me>
2017-03-04 15:55:48 -0500

    Refactor login form

--- Back-end/searchform.php
+++ Back-end/searchform.php
@@ -1,56 +1,29 @@
 <?php
 
-mysql_connect ('sql.njit.edu', 'ks492', 'SpqROrOBi');
+require_once('../functions.php');
 
-mysql_select_db ('ks492');
-
-if($_SERVER['REQUEST_METHOD'] != "POST")
+if ($_SERVER['REQUEST_METHOD'] != "POST")
 {
 	header($_SERVER['SERVER_PROTOCOL']. '405 Method Not Allowed');
 	header('Allow: POST');
 	die(json_encode(["error" => "Must Use POST"]));
 }
-session_start();
 
-$ucid=$_POST["ucid"];
-$pass=$_POST["pass"];
+$ucid = $_POST["ucid"];
+$pass = $_POST["pass"];
 
-$pw= mysql_query("SET @test=PASSWORD('$pass')");
-$gpw=mysql_query("SELECT @test");
-if($x=mysql_fetch_array($gpw)) {
-do {
-	$tmp=$x['@test'];
-} while($x=mysql_fetch_array($gpw)); }
-$result = mysql_query ("SELECT * FROM `ids` WHERE ucid ='$ucid' AND pass like '$tmp%'
-			");
-			if ($row = mysql_fetch_array($result)) {
-			do {
-				session_regenerate_id();
-				
-					$_SESSION['sess_ucid']=$row["ucid"];
-					$_SESSION['sess_id']=$row["id"];
-					$_SESSION['sess_ucid']=$row["ucid"];
-					$_SESSION['sess_name']=$row["name"];
-					$_SESSION['sess_role']=$row["role"];
-					
-				session_write_close();
-				$ex=["status" => 200, "response" => "login successful", "user_id" => $row["id"], "role" => $row["role"]];
+$result = $mysqli->query("SELECT id, role FROM users WHERE ucid = '$ucid' AND password = PASSWORD('$pass')");
 
-				echo json_encode($ex);
-							} 
-							while($row =
-							mysql_fetch_array($result));
-							} else 
-							
-							{
-							$ex=["status" => 403, "response" => "login not succesful"];
-							echo json_encode($ex);
-							}
-
-#if( $_SESSION['sess_role'] == "0"){
-# header('Location: instructor.php');
-# }else{
-#  header('Location: student.php');
-#  }
-							?>
-
+if ($row = $result->fetch_array()) {
+	echo json_encode([
+		"status" => 200,
+		"response" => "login successful",
+		"user_id" => $row["id"],
+		"role" => $row["role"],
+	]);
+} else {
+	echo json_encode([
+		"status" => 403,
+		"response" => "login not succesful",
+	]);
+}
