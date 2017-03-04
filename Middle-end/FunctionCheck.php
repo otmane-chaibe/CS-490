eb5a3b355aabf136b20402be6c46db4aceb281d7
Maurice Achtenhagen <maurice@achtenhagen.me>
2017-03-04 15:24:34 -0500

    Begin Function Checker

--- Middle-end/FunctionCheck.php
+++ Middle-end/FunctionCheck.php
@@ -2,25 +2,99 @@
 
 declare(strict_types=1);
 	
-final class FunctionCheck {
+class FunctionCheck {
 	
-	private $function_body;
+	const MAX_MODIFIERS = 2;
 	
-	private function __construct($function_body) {
-		$this->$function_body = $function_body;
-	}
+	private $function;
+	private $modifiers, $return_type, $function_name, $function_parameters, $function_body;
 	
-	public static function fromString(string $function_body) {
-		return new self($function_body);
+	function __construct($function) {
+		$this->function = trim($function);
 	}
 	
 	# Check the entire signature of the function.
-	public static function signature() {
-		throw new InvalidArgumentException("This is not a valid Java method signature.");
+	public function signature() {
+		if (empty($this->function)) {
+			throw new InvalidArgumentException("This is not a valid Java method signature.");
+		}
+		
+		$components = explode(" ", $this->function);
+		foreach($components as $c) {
+			
+		}
+		
+		$modifier = $components[0];
+		if (!self::match_modifier($modifier)) {
+			throw new BadModifierException("Illegal modifier. Expected public, private or void.");
+		}
+		
+		$return_type = $components[1];
+		$method_name = $components[2];
+		# preg_match('#\((.*?)\)#', $this->function_body, $param_str);
+		# $param_str = $param_str[1];
+		$params = [];
+		foreach (explode(",", $param_str) as $p) {
+			# var_dump(trim($p));
+			$param = explode(" ", trim($p));
+			$type = $param[0];
+			$name = $param[1];
+			$params[] = [
+				"type"     => $type,
+				"type_val" => self::get_type_from($type),
+				"name"     => $name
+			];
+		}
+//		$function = [
+//			"modifier"    => $modifier,
+//			"return_type" => $return_type,
+//			"method_name" => $method_name,
+//			"parameters"  => $params
+//		];
 	}
 	
-	# Get the number of arguments and their type.
-	public static function args() {
-		
+	public function body() {
+		preg_match('#\({.*?}\)#', $this->function_body, $body);
+	}
+	
+	# Match the function modifier (public|private|static|void)
+	private function match_modifier($mod) {
+		if (empty($mod)) { return false; }
+		switch ($mod) {
+			case "public":
+				return true;
+			case "private":
+				return true;
+			case "static":
+				return true;				
+			case "void":
+				return true;				
+			default:
+				return false;
+		}
+	}
+	
+	# Get the argument type from string
+	private function get_type_from($str) {
+		if (empty($str)) { return 0; }
+		switch ($str) {
+			case "int":
+				return 0;
+			case "float":
+				return 1;
+			case "double":
+				return 2;
+			case "string":
+				return 3;
+			case "bool":
+				return 4;											
+			default:
+				return 0;
+		}
+	}
+	
+	# String representation of self
+	public function __toString() {
+		return $this->function_body;
 	}
 }
--- Middle-end/test.php
+++ Middle-end/test.php
@@ -1,5 +1,57 @@
 <?php
+
+require_once('../functions.php');
+require_once('FunctionCheck.php');
+
+# Test 0 - Fail
+try {
+	$f_check = new FunctionCheck("");
+	$f_check->signature();
+	echo $f_check . "\n";
+} catch (InvalidArgumentException $ex) {
+	echo "Test 0 (failed): " . $ex->getMessage() . "\n";
+} catch (BadModifierException $ex) {
+	echo $ex->getMessage() . "\n";
+}
 	
-	require_once('FunctionCheck.php');
-	
-	
+# Test 1 - Pass
+$sample = "public int sum (int a, int b)";
+$f_check = new FunctionCheck($sample);
+$f_check->signature();
+echo $f_check . "\n";
+echo "Test passed\n";
+
+# Test 2 - Pass
+$sample = "public int sum(int a, int b)";
+$f_check = new FunctionCheck($sample);
+$f_check->signature();
+echo $f_check . "\n";
+echo "Test passed\n";
+
+# Test 3 - Pass 
+$sample = " public int sum(int a,int b) ";
+$f_check = new FunctionCheck($sample);
+$f_check->signature();
+echo $f_check . "\n";
+echo "Test passed\n";
+
+# Test 4 - Pass
+$sample = " public static int sum (int a,int b) ";
+$f_check = new FunctionCheck($sample);
+$f_check->signature();
+echo $f_check . "\n";
+echo "Test passed\n";
+
+# Test 5 - Fail
+$sample = "public static void int sum (int a,int b)";
+$f_check = new FunctionCheck($sample);
+$f_check->signature();
+echo $f_check . "\n";
+echo "Test passed\n";
+
+# Test 6 - Fail
+$sample = "public class static void int sum (int a,int b)";
+$f_check = new FunctionCheck($sample);
+$f_check->signature();
+echo $f_check . "\n";
+echo "Test passed\n";
