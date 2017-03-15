9c463e4df53462de5b17e06e6f22934bcfc01928
Khurshid Sohail <ks900@scarletmail.rutgers.edu>
2017-03-15 13:04:40 -0400

    added args to creating question

--- Back-end/create_question.php
+++ Back-end/create_question.php
@@ -15,6 +15,15 @@ header('Content-Type: application/json');
 
 assertPost();
 
+$user_id=$_POST['user_id'];
+$name=$_POST['function_name'];
+$category=$_POST['category'];
+$difficulty=$_POST['difficulty'];
+$type=$_POST['function_type'];
+$args=$_POST['args'];
+$description=$_POST['description'];
+
+
 #echo json_encode(Question::listAllQuestions());
-echo json_encode(Question::createQuestion());
+echo json_encode(Question::createQuestion($user_id, $name,$category, $difficulty, $type, $args, $description));
 
