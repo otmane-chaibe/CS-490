4196e07acd8b45b5c98bd18bf86bee02cbc6ebee
Khurshid Sohail <ks492@afsconnect1.njit.edu>
2017-03-28 14:06:03 -0400

    new schema & beginnng of submit_q

---
+++ Back-end/submit_question.php
@@ -0,0 +1,12 @@
+<?php
+
+#Khurshid Sohail
+
+require_once('../mysql.php');
+require_once('../functions.php');
+
+assertPost();
+
+$solution=$_POST['solution'];
+$question_id=$_POST['question_id'];
+$test_id=$_POST['test_id';]
