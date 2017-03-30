2f1f546273f6cfe7cdbb69ac1ad81291078c126e
Maurice Achtenhagen <mma93@njit.edu>
2017-03-30 08:32:11 -0400

    Add ability to add questions to tests

--- Middle-end/add_question_to_test.php
+++ Middle-end/add_question_to_test.php
@@ -6,8 +6,12 @@ require_once('../functions.php');
 
 assertPost();
 
-if (!isset($_POST['test_id']) || !isset($_POST['question_id'])) {
-	error("Missing Parameters");
+if (!isset($_POST['test_id'])) {
+	error("Missing Parameter: test_id.");
+}
+
+if (!isset($_POST['question_id'])) {
+	error("Missing Parameter: question_id.");
 }
 
 $test_id = (int) $_POST['test_id'];
