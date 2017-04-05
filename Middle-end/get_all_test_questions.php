6879b0b8409dae4d80de079842eb7c7500919169
Maurice Achtenhagen <maurice@achtenhagen.me>
2017-04-05 18:49:45 -0400

    Fix bug

--- Middle-end/get_all_test_questions.php
+++ Middle-end/get_all_test_questions.php
@@ -6,15 +6,7 @@ require_once('../functions.php');
 
 assertPost();
 
-if (!isset($_POST['test_id'])) {
-	error("Missing Parameter: test_id.");
-}
-
-$test_id = (int) $_POST['test_id'];
-
-$questions = http(BACK_END, "get_all_test_questions", [
-	"test_id" => $test_id
-]);
+$questions = http(BACK_END, "get_all_test_questions");
 
 if ($questions === false) {
 	error("cURL request failed");
