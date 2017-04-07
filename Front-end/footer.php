84c1e7d9d073dea519d5a24286c07b703c97493c
Maurice Achtenhagen <maurice@achtenhagen.me>
2017-04-07 18:57:06 -0400

    Fix ghost semicolon

--- Front-end/footer.php
+++ Front-end/footer.php
@@ -1,5 +1,5 @@
 		<!-- Maurice Achtenhagen -->
 		</main>
-		<?php if (isset($js)) { echo '<script type="application/javascript" src="js/' . $js . '.js"></script>;'; } ?>
+		<?php if (isset($js)) { echo '<script type="application/javascript" src="js/' . $js . '.js"></script>'; } ?>
 	</body>
 </html>
