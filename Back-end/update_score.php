347a8a447abf42162280909424f746b3c2177710
Khurshid Sohail <ks492@afsconnect1.njit.edu>
2017-04-09 19:30:42 -0400

    update exam score function

---
+++ Back-end/update_score.php
@@ -0,0 +1,18 @@
+<?php
+
+# Khurshid Sohail
+
+require_once('../mysql.php');
+require_once('../functions.php');
+
+assertPost();
+
+if(!isset($_POST['id']) || !isset($_POST['score']))
+{
+	error("Missing Parameters");
+}
+
+$id = (int) $_POST['id'];
+$score = (int) $_POST['score'];
+
+echo json_encode(Test::updateScore($id, $score));
