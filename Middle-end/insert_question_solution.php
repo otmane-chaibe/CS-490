42d608d1946c73d9932530a377f9db69a2060927
Maurice Achtenhagen <maurice@achtenhagen.me>
2017-04-09 20:01:57 -0400

    Add test_id column into db schema + update files

--- Middle-end/insert_question_solution.php
+++ Middle-end/insert_question_solution.php
@@ -10,6 +10,10 @@ if (!isset($_POST['user_id'])) {
 	error("Missing Parameters: user_id.");
 }
 
+if (!isset($_POST['test_id'])) {
+	error("Missing Parameters: test_id.");
+}
+
 if (!isset($_POST['q_id'])) {
 	error("Missing Parameters: q_id.");
 }
@@ -27,6 +31,7 @@ if (!isset($_POST['score'])) {
 }
 
 $user_id = (int) $_POST['user_id'];
+$test_id = (int) $_POST['test_id'];
 $question_id = (int) $_POST['q_id'];
 $solution = $_POST['solution'];
 $results = $_POST['results'];
@@ -34,6 +39,7 @@ $score = (int) $_POST['score'];
 
 $score_id = http(BACK_END, "insert_question_solution", [
 	"user_id"  => $user_id,
+	"test_id"  => $test_id,
 	"q_id"     => $question_id,
 	"solution" => $solution,
 	"results"  => $results,
