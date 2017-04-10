80100e5cb013869c53a14dcac9e235999394e357
Khurshid Sohail <ks492@afsconnect1.njit.edu>
2017-04-09 22:57:37 -0400

    changed echo stmt

--- Back-end/update_question_weight.php
+++ Back-end/update_question_weight.php
@@ -15,4 +15,5 @@ if(!isset($_POST['test_question_id']) || !isset($_POST['weight']))
 $test_question_id = (int) $_POST['test_question_id'];
 $weight = (double) $_POST['weight'];
 
-echo json_encode(Question::updateQuestionWeight($test_questions_id, $weight));
+Question::updateQuestionWeight($test_questions_id, $weight);
+echo json_encode(true);
