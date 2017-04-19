15c30794ecc41f2e3c8a3cee43d2fb5f4bdb41d3
Khurshid Sohail <ks492@afsconnect1.njit.edu>
2017-04-19 16:41:11 -0400

    added test_id param

--- Back-end/update_score.php
+++ Back-end/update_score.php
@@ -7,12 +7,15 @@ require_once('../functions.php');
 
 assertPost();
 
-if(!isset($_POST['id']) || !isset($_POST['score']))
+if(!isset($_POST['id']) || !isset($_POST['test_id']) || !isset($_POST['score']))
 {
 	error("Missing Parameters");
 }
 
 $id = (int) $_POST['id'];
 $score = (int) $_POST['score'];
+$test_id = (int) $_POST['score'];
 
-echo json_encode(Test::updateQuestionScore($id, $score));
+echo json_encode(Test::updateQuestionScore($id, $test_id, $score));
+
+
